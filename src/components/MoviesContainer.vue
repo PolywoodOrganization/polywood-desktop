bb8870d76cdff60612b335d631f453ecb1e0de8e
Valentin Berger <Valentin.berger38@gmail.com>
2019-10-24 15:35:39 +0200

    :lipstick: Add pagination at the bottom of `MoviesContainer`

--- src/components/MoviesContainer.vue
+++ src/components/MoviesContainer.vue
@@ -1,12 +1,27 @@
 <template>
 	<div class="container movies-container">
 		<div class="row justify-content-center" v-if="this.$store.getters.movies.length !== 0">
-			<Movie class="movie" v-for="movie in this.$store.getters.movies" v-bind="movie" :key="movie.id"/>
+			<Movie class="movie" v-for="movie in this.$store.getters.movies" v-bind="movie" :key="movie.id" />
 		</div>
+
 		<div class="row justify-content-center" v-else>
 			<Card additional-card-class="border-warning bg-warning empty-movie" title="Aucun film trouvé 😕">
-				<img src="../assets/img/clapper.png" alt="Aucun film disponible" title="Aucun film disponible"/>
+				<img src="../assets/img/clapper.png" alt="Aucun film disponible" title="Aucun film disponible" />
 			</Card>
+		</div>
+
+		<div class="row">
+			<div class="col-12">
+				<ul class="pagination pagination-lg justify-content-center">
+					<li class="page-item disabled"><a class="page-link" href="#">&laquo;</a></li>
+					<li class="page-item active"><a class="page-link" href="#">1</a></li>
+					<li class="page-item"><a class="page-link" href="#">2</a></li>
+					<li class="page-item"><a class="page-link" href="#">3</a></li>
+					<li class="page-item"><a class="page-link" href="#">4</a></li>
+					<li class="page-item"><a class="page-link" href="#">5</a></li>
+					<li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+				</ul>
+			</div>
 		</div>
 	</div>
 </template>
@@ -29,5 +44,9 @@ export default {
 
 .movie {
 	display: inline-block;
+}
+
+.page-item a {
+	text-decoration: none;
 }
 </style>
